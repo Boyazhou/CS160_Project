131460f91f01adb839ab2252843b9044d3ef2abb
Boyazhou <zhouboya2012@gmail.com>
2015-11-20 18:48:38 -0800

    Update Course.java

--- data/futurelearn_scraper/Course.java
+++ data/futurelearn_scraper/Course.java
@@ -10,15 +10,33 @@ public class Course {
 	private ArrayList<String> courseLengths;
 	private String category="";
 	private ArrayList<String> courseImages;
+	private String shortDesc="";
+	private String university="";
+	private String videoLink="";
+	private String certificate="";
+	private int course_fee=0;
+	private String language="English";
+	private String longDesc= "";
+	private int course_id;
+	public String site="www.futurelearn.com";
 	
 	public Course(){
 		instructorNames = new ArrayList<String>();
 		instructorImages = new ArrayList<String>();
 		startDates = new ArrayList<String>();
 		courseLengths = new ArrayList<String>();
-		courseImages = new ArrayList<String>();
-		
+		courseImages = new ArrayList<String>();	
 	}
+	
+	public Course(int course_id){
+		instructorNames = new ArrayList<String>();
+		instructorImages = new ArrayList<String>();
+		startDates = new ArrayList<String>();
+		courseLengths = new ArrayList<String>();
+		courseImages = new ArrayList<String>();
+		this.setCourseId(course_id);
+	}
+	
 	public String getUrl() {
 		return url;
 	}
@@ -67,15 +85,69 @@ public class Course {
 	public String getCourseImages(){
 		return arraylistToString(courseImages);
 	}
+	public String getShortDesc(){
+		return shortDesc;
+	}
+	public void setShortDesc(String shortDesc){
+		this.shortDesc = shortDesc;
+	}
+	public String getUniversity(){
+		return university;
+	}
+	public void setUniversity(String university){
+		this.university = university;
+	}
+	public String getVideoLink(){
+		return videoLink;
+	}
+	public void setVideoLink(String videoLink){
+		this.videoLink = videoLink;
+	}
+    public void setCertificate(String certificate){
+       this.certificate = certificate;
+    }
+    public String getCertificate(){
+       return certificate;
+    }
+	public int getCourse_fee(){
+		return course_fee;
+	}
+	public String getLanguage(){
+		return language;
+	}
+    public void setLongDescription(String longdescription){
+        this.longDesc = longdescription;
+    }
+    public String getLongDescription(){
+        return longDesc;
+    }
+    public void setCourseId(int course_id){
+    	this.course_id = course_id;
+    }
+    public int getCourseId(){
+    	return course_id;
+    }
+	
+	
+	
 	@Override
 	public String toString(){
-		return "Course Name: "+ courseName + "\n"
-				+"Category: "+ category + "\n"
-				+"Course Images: "+ courseImages + "\n"
-				+"Course URL: "+ url + "\n"
-				+"Instructor(s): "+ instructorNames + "\n"
+		return  "Course Id: " + course_id + "\n"
+				+"Title: "+ courseName + "\n"
+				+"Short_desc" + shortDesc + "\n"
+				+"Long_desc" + longDesc + "\n"
+				+"Course_link" + url + "\n"
+				+"Video_link" + videoLink + "\n"
 				+"Start Date: "+ startDates.get(0) + "\n"
-				+"Duration: "+ courseLengths.get(0) +" weeks"+ "\n";
+				+"Course Length: "+ courseLengths.get(0) +" weeks"+ "\n"
+				+"Course Images: "+ courseImages + "\n"
+				+"Category: "+ category + "\n"
+				+"Site: " +  site + "\n"
+				+"Course fee: " + course_fee + "\n"
+				+"Language: " + language + "\n"
+				+"Certificate: " + certificate + "\n"
+				+"University: " + university + "\n";
+	//			+"Time_scraped: "+ 	
 	}
 	
 	//convert a list of string to a string 
